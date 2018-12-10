5a05b56202a8c919ffbf3dc14c740e4a3677ad74
SeungukLee <lsy931106@gmail.com>
2018-12-10 10:19:19 +0900

    [Exercise 2-1] Test code

--- src/test/scala/ch2/Ch2Suite.scala
+++ src/test/scala/ch2/Ch2Suite.scala
@@ -1,5 +1,23 @@
 package ch2
 
-class Ch2Suite {
+import org.scalatest.FunSuite
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class Ch2Suite extends FunSuite {
+
+  import Ch2._
+
+  test("[exercise 2-1]") {
+    assert(fib(0) === 0)
+    assert(fib(1) === 1)
+    assert(fib(2) === 1)
+    assert(fib(3) === 2)
+    assert(fib(4) === 3)
+    assert(fib(5) === 5)
+    assert(fib(6) === 8)
+  }
 }
